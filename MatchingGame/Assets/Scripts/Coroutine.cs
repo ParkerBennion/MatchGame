7a2878e3347196aec48bddd1458c7ae657e66ad7
ParkerBennion <parkerbennion@gmail.com>
2022-09-15 12:15:34 -0600

    ui anim coroutines

--- MatchingGame/Assets/Scripts/Coroutine.cs
+++ MatchingGame/Assets/Scripts/Coroutine.cs
@@ -1,23 +1,45 @@
-using System;
 using System.Collections;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UIElements;
 
 public class Coroutine : MonoBehaviour
 {
+    public UnityEvent repeatEvent,startEvent, endEvent;
+    
     public int waitTime = 3;
-    private WaitForSeconds wait;
     public int counterNum = 3;
-    public UnityEvent repeatEvent;
+    private WaitForSeconds wait;
+    public TextMeshProUGUI textObj;
+    
     
     private IEnumerator StartCounter()
     {
         wait = new WaitForSeconds(waitTime);
         
-        while (counterNum > 0)
+        while (counterNum > -1)
         {
-            yield return wait;
-            repeatEvent.Invoke();
+            if (counterNum >=0)
+            {
+                textObj.text = counterNum.ToString();
+                
+                counterNum--;
+                if (counterNum == -1)
+                {
+                    textObj.text = "GO!!!";
+                }
+                Debug.Log(counterNum);
+                repeatEvent.Invoke();
+                yield return wait;
+            }
         }
+        counterNum = 3;
+        endEvent.Invoke();
+    }
+
+    public void StartCourouteneCounter()
+    {
+        StartCoroutine(StartCounter());
     }
 }
